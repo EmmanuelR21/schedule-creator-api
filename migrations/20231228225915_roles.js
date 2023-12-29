4be875ccb2675fb4d2b602d3fb355701f678d93c
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2023-12-29 11:35:59 -0500

    migrations for roles and users

---
+++ migrations/20231228225915_roles.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('roles', function (table) {
+        table.integer('user_id').references('id').inTable('users');
+        table.integer('server_id');
+        table.integer('admin_id');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema
+        .dropTable('roles');
+};
