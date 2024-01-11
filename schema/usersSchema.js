c77381cfed0df72129cef185012baf77cc5ebda2
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-01-11 09:03:11 -0500

    added schema and validation for users table

---
+++ schema/usersSchema.js
@@ -0,0 +1,21 @@
+const Joi = require("joi")
+
+const signUpSchema = Joi.object({
+    username: Joi.string().min(8).max(16).required().messages({
+        "string.min": "Username must be atleast 8 characters!",
+        "string.max": "Username must be less than 16 characters!",
+        "string.empty": "Username is required!"
+    }),
+    password: Joi.string().min(10).max(30).required().messages({
+        "string.min": "Password must be atleast 10 characters!",
+        "string.max": "Password must be less than 30 characters!",
+        "string.empty": "Password is required!"
+    }),
+    confirmPassword: Joi.valid(Joi.ref("password")).required().messages({
+        "any.only": "Passwords must match!"
+    })
+})
+
+module.exports = {
+    signUpSchema
+}
---
+++ validation/usersValidations.js
@@ -0,0 +1,16 @@
+const signUpValidation = (schema) => {
+    return (req, res, next) => {
+        const result = schema.validate(req.body)
+        if (result.error) {
+            return res.status(400).json({
+                error: result.error.details[0].message
+            })
+        }
+
+        next()
+    }
+}
+
+module.exports = {
+    signUpValidation
+}
