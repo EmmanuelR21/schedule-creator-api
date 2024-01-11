e638e0588b0faf1c70575b68d34e93496928e6e9
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-01-11 09:07:52 -0500

    added schema validation middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require('cors');
 const { signUp, doesUserExist } = require("./controllers/usersController");
+const { signUpSchema } = require("./schema/usersSchema");
+const { signUpValidation } = require("./validation/usersValidations");
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/getUser/:username', doesUserExist)
-app.post('/signUp', signUp);
+app.post('/signUp', signUpValidation(signUpSchema), signUp);
 
 app.listen(3000);
